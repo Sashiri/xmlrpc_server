03e1f1e3a058c9ffa1b75dd44921919ac75425b6
Sashiri <ali.reanc@gmail.com>
2019-12-26 14:20:43 +0100

    InitCommit

---
+++ example/xmlrpc_server_example.dart
@@ -0,0 +1,6 @@
+import 'package:xmlrpc_server/xmlrpc_server.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/xmlrpc_server_test.dart
@@ -0,0 +1,16 @@
+import 'package:xmlrpc_server/xmlrpc_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
